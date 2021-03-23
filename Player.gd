a89733d78aa369d56f902b7e5caebb576f55c566
Sabrina Yang <syang75@go.pasadena.edu>
2021-03-22 20:08:56 -0700

    gave player object gravity

---
+++ Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+export (int) var jump_speed = -400
+export (int) var gravity = 1200
+
+var velocity = Vector2()
+var jumping = false
+
+func get_input():
+	var jump = Input.is_action_just_pressed('ui_up')
+
+	if jump and is_on_floor():
+		jumping = true
+		velocity.y = jump_speed
+
+func _physics_process(delta):
+	get_input()
+	velocity.y += gravity * delta
+	if jumping and is_on_floor():
+		jumping = false
+	velocity = move_and_slide(velocity, Vector2(0, -1))
